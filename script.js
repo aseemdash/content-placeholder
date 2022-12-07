ed4e0e5a16eda8ee37facd993778d3615340216a
Aseem Amitav Dash <91455585+aseemdash@users.noreply.github.com>
2022-12-07 13:26:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const header = document.getElementById('header');
+const title = document.getElementById('title');
+const excerpt = document.getElementById('excerpt');
+const profile_img = document.getElementById('profile_img');
+const name = document.getElementById('name');
+const date = document.getElementById('date');
+
+const animated_bgs = document.querySelectorAll('animated-bg');
+const animated_bg_texts = document.querySelectorAll('animated-bg-text');
+
+setTimeout(getData, 2500);
+
+function getData() {
+  header.innerHTML =
+    ' <img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80" alt="" />';
+  title.innerHTML = 'Front End Developer';
+  excerpt.innerHTML = 'This is the coolest job for anyone. Very Exciting!';
+  profile_img.innerHTML =
+    ' <img src="https://randomuser.me/api/portraits/men/10.jpg" alt="" />';
+  name.innerHTML = 'Henry Cavill';
+  date.innerHTML = '8th Oct, 1993';
+  animated_bgs.forEach(bg => bg.classList.remove('animated-bg'));
+  animated_bg_texts.forEach(bg => bg.classList.remove('animated-bg-text'));
+}
